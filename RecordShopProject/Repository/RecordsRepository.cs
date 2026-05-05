c37cb5436277d43a7d40bcad12d75387445ca76e
Marcus Paterson <marcus_paterson@outlook.com>
2026-05-05 16:11:56 +0100

    FEAT: Created Repository level and completed one successful test for the repository level

---
+++ RecordShopProject/Repository/RecordsRepository.cs
@@ -0,0 +1,25 @@
+﻿using RecordShopProject.DataModels;
+using System.Text.Json;
+
+namespace RecordShopProject.Repository
+{
+    public interface IRecordsRepository
+    { 
+        List<Record> GetAllRecords();
+    }
+
+    public class RecordsRepository : IRecordsRepository
+    {
+        private readonly RecordShopDBContext _context;
+        public RecordsRepository(RecordShopDBContext context)
+        {
+            _context = context;
+        }
+        public List<Record> GetAllRecords()
+        {
+            return _context.Records.ToList();
+        }
+
+
+    }
+}
---
+++ RecordShopSolution.Tests/RepositoryTests/RecordRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using RecordShopProject.DataModels;
+using RecordShopProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordShopProject.Tests
+{
+    public class RecordRepositoryTests
+    {
+
+        [Test]
+        public void GetAllRecords_ShouldReturnListOfRecords()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<RecordShopDBContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+
+            using var context = new RecordShopDBContext(options);
+
+            context.Records.AddRange(
+                new Record { RecordId = 1, Title = "Test Album 1", Artist = "Test Artist 1", Genre = "Rock", Year = 2000, Price = 10, Stock = 5 },
+                new Record { RecordId = 2, Title = "Test Album 2", Artist = "Test Artist 2", Genre = "Pop", Year = 2005, Price = 15, Stock = 3 }
+            );
+            context.SaveChanges();
+
+            var repository = new RecordsRepository(context);
+
+            // Act
+            var records = repository.GetAllRecords();
+
+            // Assert
+            Assert.AreEqual(2, records.Count);
+        }
+    }
+}
