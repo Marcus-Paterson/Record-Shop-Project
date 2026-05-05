d5a9c049997b1f705842d1116c4e0d6a1e092dcb
Marcus Paterson <marcus_paterson@outlook.com>
2026-05-05 16:50:45 +0100

    FEAT: Created Service level and RecordService Test. Test passed

---
+++ RecordShopProject/Service/RecordsService.cs
@@ -0,0 +1,24 @@
+﻿using RecordShopProject.DataModels;
+using RecordShopProject.Repository;
+
+namespace RecordShopProject.Service
+{
+    public interface IRecordsService
+    {
+        List<Record> GetAllRecords();
+    }
+    public class RecordsService : IRecordsService
+    {
+        private readonly IRecordsRepository _recordRepository;
+        public RecordsService(IRecordsRepository repository)
+        {
+            _recordRepository = repository;
+        }
+        public List<Record> GetAllRecords()
+        {
+            return _recordRepository.GetAllRecords();
+        }
+
+
+    }
+}
---
+++ RecordShopSolution.Tests/ServiceTests/RecordServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecordShopProject.Repository;
+using RecordShopProject.Service;
+using RecordShopProject.DataModels;
+
+namespace RecordShopProject.Tests.ServiceTests
+{
+    public class RecordServiceTests
+    {
+        private Mock<IRecordsRepository> _recordRepositoryMock;
+        private RecordsService _recordService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _recordRepositoryMock = new Mock<IRecordsRepository>();
+            _recordService = new RecordsService(_recordRepositoryMock.Object);
+        }
+        [Test]
+        public void GetAllRecords_ReturnsAllRecords()
+        {
+            // Arrange
+            var testRecords = new List<Record>
+            {
+                new Record { RecordId = 1, Title = "Test Album 1", Artist = "Test Artist 1", Genre = "Rock", Year = 2000, Price = 10, Stock = 5 },
+                new Record { RecordId = 2, Title = "Test Album 2", Artist = "Test Artist 2", Genre = "Pop", Year = 2005, Price = 15, Stock = 3 }
+            };
+            _recordRepositoryMock.Setup(repo => repo.GetAllRecords()).Returns(testRecords);
+            // Act
+            var result = _recordService.GetAllRecords();
+            // Assert
+            Assert.That(result, Is.EqualTo(testRecords));
+        }
+
+
+    }
+}
